9eac508cc7e12c14c0ad1fd5a757ee4040a3ab5a
Kerpackie <81828282+Kerpackie@users.noreply.github.com>
2021-10-28 20:51:05 +0100

    Add DbContext
    This commit will add the DbContext & DbContextFactory for creating and managing connections to our Database.

---
+++ Events.Data/Context/EventDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Events.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Events.Data.Context
+{
+    public class EventDbContext : DbContext
+    {
+        public EventDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+        
+        public DbSet<Event> Events { get; set; }
+        public DbSet<PermittedRole> PermittedRoles { get; set; }
+    }
+}
---
+++ Events.Data/Context/EventDbContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Events.Data.Context
+{
+    public class EventDbContextFactory : IDesignTimeDbContextFactory<EventDbContext>
+    {
+        public EventDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, true)
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder()
+                .UseMySql(
+                    configuration["EventsDatabase"],
+                    new MySqlServerVersion(new Version(8, 0, 21)));
+
+            return new EventDbContext(optionsBuilder.Options);
+        }
+    }
+}
