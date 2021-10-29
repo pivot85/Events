4ad92f646c7a58eed5b4030c47573352b4b54e21
Directoire <hendrikdemir@protonmail.com>
2021-10-30 00:29:03 +0200

    Basic implementation of newevent, working on permissions and i.e. control panels

--- Events.Bot/Modules/EventModule.cs
+++ Events.Bot/Modules/EventModule.cs
@@ -1,6 +1,8 @@
 ﻿using Discord;
 using Discord.Commands;
+using Discord.Rest;
 using Discord.WebSocket;
+using Events.Bot.Utils;
 using Events.Data.DataAccessLayer;
 using Interactivity;
 using System;
@@ -37,7 +39,7 @@ namespace Events.Bot.Modules
                 var response = await Interactivity.NextMessageAsync(x => x.Author.Id == Context.User.Id);
                 if (response.IsTimeouted || response.Value == null)
                 {
-                    await ReplyAsync("You didn't respond in time, please run the command again.");
+                    await Context.Channel.SendMessageAsync("You didn't respond in time, please run the command again.");
                     return;
                 }
 
@@ -45,13 +47,13 @@ namespace Events.Bot.Modules
 
                 if (content.ToLower() == "cancel")
                 {
-                    await ReplyAsync("The setup was cancelled.");
+                    await Context.Channel.SendMessageAsync("The setup was cancelled.");
                     return;
                 }
 
                 if (content.Length > TITLE_CHAR_LIMIT)
                 {
-                    await ReplyAsync($"Please pick a title that is shorter than {TITLE_CHAR_LIMIT} characters.");
+                    await Context.Channel.SendMessageAsync($"Please pick a title that is shorter than {TITLE_CHAR_LIMIT} characters.");
                     continue;
                 }
 
@@ -59,14 +61,14 @@ namespace Events.Bot.Modules
                 break;
             }
 
-            await ReplyAsync($"Title set! What description would you like the event to have? It must be shorter than {DESC_CHAR_LIMIT} characters.");
+            await Context.Channel.SendMessageAsync($"Title set! What description would you like the event to have? It must be shorter than {DESC_CHAR_LIMIT} characters.");
             string description = string.Empty;
             while (true)
             {
                 var response = await Interactivity.NextMessageAsync(x => x.Author.Id == Context.User.Id);
                 if (response.IsTimeouted || response.Value == null)
                 {
-                    await ReplyAsync("You didn't respond in time, please run the command again.");
+                    await Context.Channel.SendMessageAsync("You didn't respond in time, please run the command again.");
                     return;
                 }
 
@@ -74,13 +76,13 @@ namespace Events.Bot.Modules
 
                 if (content.ToLower() == "cancel")
                 {
-                    await ReplyAsync("The setup was cancelled.");
+                    await Context.Channel.SendMessageAsync("The setup was cancelled.");
                     return;
                 }
 
                 if (content.Length > DESC_CHAR_LIMIT)
                 {
-                    await ReplyAsync($"Please pick a description that is shorter than {DESC_CHAR_LIMIT} characters.");
+                    await Context.Channel.SendMessageAsync($"Please pick a description that is shorter than {DESC_CHAR_LIMIT} characters.");
                     continue;
                 }
 
@@ -88,14 +90,14 @@ namespace Events.Bot.Modules
                 break;
             }
 
-            await ReplyAsync("Description set! When should the event take place? Please format it as mm/dd/yyyy HH:MM:SS");
+            await Context.Channel.SendMessageAsync("Description set! When should the event take place? Please format it as mm/dd/yyyy HH:MM:SS");
             DateTime start;
             while (true)
             {
                 var response = await Interactivity.NextMessageAsync(x => x.Author.Id == Context.User.Id);
                 if (response.IsTimeouted || response.Value == null)
                 {
-                    await ReplyAsync("You didn't respond in time, please run the command again.");
+                    await Context.Channel.SendMessageAsync("You didn't respond in time, please run the command again.");
                     return;
                 }
 
@@ -103,26 +105,26 @@ namespace Events.Bot.Modules
 
                 if (content.ToLower() == "cancel")
                 {
-                    await ReplyAsync("The setup was cancelled.");
+                    await Context.Channel.SendMessageAsync("The setup was cancelled.");
                     return;
                 }
 
                 if (!DateTime.TryParse(content, out start))
                 {
-                    await ReplyAsync($"Please provide a properly formatted date and time for the start of the event.");
+                    await Context.Channel.SendMessageAsync($"Please provide a properly formatted date and time for the start of the event.");
                     continue;
                 }
                 break;
             }
 
-            await ReplyAsync($"The event will start on {string.Format("{0:f}", start)}. How long will the event last? Please provide the duration as HH:MM:SS.");
+            await Context.Channel.SendMessageAsync($"The event will start on {string.Format("{0:f}", start)}. How long will the event last? Please provide the duration as HH:MM:SS.");
             TimeSpan duration;
             while (true)
             {
                 var response = await Interactivity.NextMessageAsync(x => x.Author.Id == Context.User.Id);
                 if (response.IsTimeouted || response.Value == null)
                 {
-                    await ReplyAsync("You didn't respond in time, please run the command again.");
+                    await Context.Channel.SendMessageAsync("You didn't respond in time, please run the command again.");
                     return;
                 }
 
@@ -130,66 +132,163 @@ namespace Events.Bot.Modules
 
                 if (content.ToLower() == "cancel")
                 {
-                    await ReplyAsync("The setup was cancelled.");
+                    await Context.Channel.SendMessageAsync("The setup was cancelled.");
                     return;
                 }
 
                 if (!TimeSpan.TryParse(content, out duration))
                 {
-                    await ReplyAsync($"Please provide a properly formatted duration for the event.");
+                    await Context.Channel.SendMessageAsync($"Please provide a properly formatted duration for the event.");
                     continue;
                 }
                 break;
             }
 
-            //await ReplyAsync($"The event will last for {(duration.TotalHours > 0 ? $"{duration.Hours} hour(s) and {duration.Minutes} minute(s)" : $"{duration.Minutes} minutes")}. Do you want to add any stewards? Please mention them or respond with \"skip\".");
-            //ulong stewardRole;
-            //while (true)
-            //{
-            //    var response = await Interactivity.NextMessageAsync(x => x.Author.Id == Context.User.Id);
-            //    if (response.IsTimeouted || response.Value == null)
-            //    {
-            //        await ReplyAsync("You didn't respond in time, cancelled the setup.");
-            //        return;
-            //    }
+            var eventId = Guid.NewGuid();
+            var eventIdSubstring = eventId.ToString().Substring(0, 7);
 
-            //    string content = response.Value.Content;
+            await Context.Channel.SendMessageAsync($"The event will last for {(duration.TotalHours > 1 ? $"{duration.Hours} hour(s) and {duration.Minutes} minute(s)" : $"{duration.Minutes} minutes")}. Do you want to add any stewards? Please mention them or respond with \"skip\".");
+            ulong stewardRoleId = 0;
+            RestRole stewardRole = null;
+            while (true)
+            {
+                var response = await Interactivity.NextMessageAsync(x => x.Author.Id == Context.User.Id);
+                if (response.IsTimeouted || response.Value == null)
+                {
+                    await Context.Channel.SendMessageAsync("You didn't respond in time, cancelled the setup.");
+                    return;
+                }
 
-            //    if (content.ToLower() == "cancel")
-            //    {
-            //        await ReplyAsync("The setup was cancelled.");
-            //        return;
-            //    }
+                string content = response.Value.Content;
 
-            //    if (content.ToLower() == "skip")
-            //        break;
+                if (content.ToLower() == "cancel")
+                {
+                    await Context.Channel.SendMessageAsync("The setup was cancelled.");
+                    return;
+                }
 
-            //    var users = new List<SocketGuildUser>();
-            //    foreach (var mention in content.Split(" "))
-            //    {
-            //        if (!MentionUtils.TryParseUser(mention, out ulong userId))
-            //            continue;
+                stewardRole = await Context.Guild.CreateRoleAsync(eventIdSubstring + " Steward", isHoisted: false, isMentionable: false);
+                stewardRoleId = stewardRole.Id;
 
-            //        var user = Context.Guild.GetUser(userId);
-            //        if (user == null || users.Contains(user))
-            //            continue;
+                if (content.ToLower() == "skip")
+                    break;
 
-            //        if (stewardRole == null)
-            //        {
-            //            var newRole = await Context.Guild.CreateRoleAsync(title.Take(8))
-            //        }
+                var users = new List<SocketGuildUser>();
+                foreach (var mention in content.Split(" "))
+                {
+                    if (!MentionUtils.TryParseUser(mention, out ulong userId))
+                        continue;
 
-            //        allowedRoles.Add(role.Id);
-            //    }
+                    var user = Context.Guild.GetUser(userId);
+                    if (user == null || users.Contains(user))
+                        continue;
 
-            //    if (allowedRoles.Count == 0)
-            //    {
-            //        await ReplyAsync("There were no (valid) roles provided, please try again.");
-            //        continue;
-            //    }
+                    await user.AddRoleAsync(stewardRole);
+                    users.Add(user);
+                }
 
-            //    break;
-            //}
+                break;
+            }
+
+            await Context.Channel.SendMessageAsync($"Stewards have been set! Do you want to add any speakers? Please mention them or respond with \"skip\".");
+            ulong speakerRoleId = 0;
+            RestRole speakerRole = null;
+            while (true)
+            {
+                var response = await Interactivity.NextMessageAsync(x => x.Author.Id == Context.User.Id);
+                if (response.IsTimeouted || response.Value == null)
+                {
+                    await Context.Channel.SendMessageAsync("You didn't respond in time, cancelled the setup.");
+                    return;
+                }
+
+                string content = response.Value.Content;
+
+                if (content.ToLower() == "cancel")
+                {
+                    await stewardRole.DeleteAsync();
+                    await Context.Channel.SendMessageAsync("The setup was cancelled.");
+                    return;
+                }
+
+                speakerRole = await Context.Guild.CreateRoleAsync(eventIdSubstring + " Speaker", isHoisted: false, isMentionable: false);
+                speakerRoleId = stewardRole.Id;
+
+                if (content.ToLower() == "skip")
+                    break;
+
+                var users = new List<SocketGuildUser>();
+                foreach (var mention in content.Split(" "))
+                {
+                    if (!MentionUtils.TryParseUser(mention, out ulong userId))
+                        continue;
+
+                    var user = Context.Guild.GetUser(userId);
+                    if (user == null || users.Contains(user))
+                        continue;
+
+                    await user.AddRoleAsync(speakerRole);
+                    users.Add(user);
+                }
+
+                break;
+            }
+
+            ulong cosmeticRoleId = 0;
+            if ((Context.User as SocketGuildUser).GuildPermissions.Administrator)
+            {
+                await Context.Channel.SendMessageAsync($"Is there a role you'd like to use as a cosmetic role? Please mention it or respond with \"skip\".");
+                while (true)
+                {
+                    var response = await Interactivity.NextMessageAsync(x => x.Author.Id == Context.User.Id);
+                    if (response.IsTimeouted || response.Value == null)
+                    {
+                        await Context.Channel.SendMessageAsync("You didn't respond in time, cancelled the setup.");
+                        return;
+                    }
+
+                    string content = response.Value.Content;
+
+                    if (content.ToLower() == "cancel")
+                    {
+                        await stewardRole.DeleteAsync();
+                        await speakerRole.DeleteAsync();
+                        await Context.Channel.SendMessageAsync("The setup was cancelled.");
+                        return;
+                    }
+
+                    if (content.ToLower() == "skip")
+                        break;
+
+                    if (!MentionUtils.TryParseRole(content, out ulong roleId))
+                    {
+                        await Context.Channel.SendMessageAsync($"Please provide a valid mention of a role.");
+                        continue;
+                    }
+
+                    var role = Context.Guild.GetRole(roleId);
+                    if (role == null)
+                    {
+                        await Context.Channel.SendMessageAsync("That role does not exist, please provide a valid role.");
+                        continue;
+                    }
+
+                    cosmeticRoleId = role.Id;
+                    break;
+                }
+            }
+
+            var message = await Context.Channel.SendMessageAsync("All set! Time to create the event...");
+            var attendeeRole = await Context.Guild.CreateRoleAsync(eventIdSubstring + " Attendee", isHoisted: false, isMentionable: false);
+
+            var category = await Context.Guild.CreateCategoryChannelAsync("Event " + eventIdSubstring);
+            var textChannel = await Context.Guild.CreateTextChannelAsync(eventIdSubstring + "-general", x => x.CategoryId = category.Id);
+            var voiceChannel = await Context.Guild.CreateVoiceChannelAsync("Event " + eventIdSubstring, x => x.CategoryId = category.Id);
+            var controlChannel = await Context.Guild.CreateTextChannelAsync(eventIdSubstring + "-control", x => x.CategoryId = category.Id);
+
+            await EventsDataAccessLayer.Create(eventId, Context.Guild.Id, Context.User.Id, title, start, duration, category.Id, textChannel.Id, voiceChannel.Id, controlChannel.Id, stewardRoleId, speakerRoleId, attendeeRole.Id, cosmeticRoleId, false);
+
+            await message.ModifyAsync(x => x.Content = $"Done! All roles and channels have been created for the event.");
         }
     }
 }
